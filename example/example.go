07a566f3df6d73c01f166c7ee947eeec9c3f1e36
Daniel Brain <github@biboop.com>
2012-05-29 08:25:16 +1000

    Update example, it now 'ates everything

--- example/example.go
+++ example/example.go
@@ -6,10 +6,14 @@ import (
 )
 
 func main() {
-  server := soggy.NewServer("/web");
+  server := soggy.NewServer("/");
   server.Get("/i/like/cheese", func (req *soggy.Request, res *soggy.Response, env *soggy.Env, next func(error)) {
     res.Header().Set("Content-Type", "text/plain")
     res.Write([]byte("This is an example server. Hell yeah. It likes cheese"))
+  })
+  server.Get("/i/ate/it/blah.html", func (req *soggy.Request, res *soggy.Response, env *soggy.Env, next func(error)) {
+    res.Header().Set("Content-Type", "text/html")
+    res.Write([]byte("<html><body>It ates you too</body></html>"))
   })
   server.Get("/", func (req *soggy.Request, res *soggy.Response, env *soggy.Env, next func(error)) {
     res.Header().Set("Content-Type", "text/plain")
