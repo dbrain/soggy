207eb756d728e7536d1ea39099e9e7c2bea980f2
Tom Lee <github@tomlee.co>
2012-05-28 02:23:41 -0700

    Added Response.SetHeader(...)

--- response.go
+++ response.go
@@ -17,6 +17,10 @@ func (self *Response) WriteString(s string) (int, error) {
     return self.Write([]byte(s))
 }
 
+func (self *Response) SetHeader(header, value string) {
+    self.Header().Set(header, value)
+}
+
 func NewResponse(res http.ResponseWriter) *Response {
   wrappedResponse := &Response{res}
   wrappedResponse.Header().Set(POWERED_BY_HEADER, POWERED_BY)
