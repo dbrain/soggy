1726036a74b1a8d26febc165fc1ca91bf8a0e027
Daniel Brain <github@biboop.com>
2013-05-05 15:24:47 +1000

    Improve BindHandlers so routing priority works

--- soggy.go
+++ soggy.go
@@ -38,11 +38,9 @@ func (app *App) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 }
 
 func (app *App) BindHandlers() {
-  for _, server := range app.servers {
-    http.HandleFunc(server.Mountpoint, func (res http.ResponseWriter, req *http.Request) {
-      server.ServeHTTP(res, req)
-    })
-  }
+  http.HandleFunc("/", func (res http.ResponseWriter, req *http.Request) {
+    app.ServeHTTP(res, req)
+  })
 }
 
 func (app *App) Listen(address string) {
