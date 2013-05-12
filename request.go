e113270bb44f7c0640d1cd5ebed87bc07a382646
Daniel Brain <github@biboop.com>
2013-05-12 12:04:22 +1000

    Add basic (JSON only) body parsing, req.GetBody()

--- request.go
+++ request.go
@@ -3,6 +3,10 @@ package soggy
 import (
   "net/http"
   "net/url"
+  "strings"
+  "encoding/json"
+  "errors"
+  "io/ioutil"
 )
 
 type URLParams []string
@@ -12,7 +16,14 @@ type Request struct {
   ID string
   RelativePath string
   URLParams URLParams
+
+  bodyParsed bool
+  bodyType string
+  parsedBody interface{}
+  bodyParseError error
 }
+
+var BodyTypeJson = "json"
 
 func (req *Request) SetRelativePath(mountpoint string, path string) {
   if mountpoint == "/" {
@@ -21,6 +32,44 @@ func (req *Request) SetRelativePath(mountpoint string, path string) {
     relativePath := path[len(mountpoint)-1:len(path)]
     req.RelativePath = relativePath
   }
+}
+
+func (req *Request) GetBody() (string, interface{}, error) {
+  if req.bodyParsed {
+    return req.bodyType, req.parsedBody, req.bodyParseError
+  }
+
+  req.bodyParsed = true
+  contentType := req.Header.Get("Content-Type")
+  if contentType == "" {
+    req.bodyParseError = errors.New("No content type specified")
+    return "", nil, req.bodyParseError
+  }
+
+  contentTypeParts := strings.Split(contentType, ";")
+  mimeType := contentTypeParts[0]
+  switch {
+  case strings.HasPrefix(mimeType, "application/") && strings.HasSuffix(mimeType, "json"):
+    req.bodyType = BodyTypeJson
+    return req.parseJSON()
+  }
+
+  return "", nil, errors.New("Unsupported content type " + contentType)
+}
+
+func (req *Request) parseJSON() (string, interface{}, error) {
+  var parsedBody map[string]interface{}
+  body, err := ioutil.ReadAll(req.Body);
+  if err != nil {
+    req.bodyParseError = err
+    return req.bodyType, nil, req.bodyParseError
+  }
+  if err := json.Unmarshal(body, &parsedBody); err != nil {
+    req.bodyParseError = err
+    return req.bodyType, nil, req.bodyParseError
+  }
+  req.parsedBody = parsedBody
+  return BodyTypeJson, req.parsedBody, nil
 }
 
 func NewRequest(req *http.Request) *Request {
@@ -35,4 +84,3 @@ func newStubRequest(method, path string) *Request {
   req := &http.Request{ URL: url, Method: method }
   return &Request{ ID: UIDString(), Request: req, RelativePath: path }
 }
-
